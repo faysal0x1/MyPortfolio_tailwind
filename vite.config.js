26c38f575cf8b1493377027fc6b13832c7ee1a07
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-05-29 02:16:08 +0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-const { resolve } = require('path')
+const { resolve } = require("path");
 export default {
-    plugins: [],
-    server: {
-        open: '/index.html',
-        port: 3116,
+  plugins: [],
+  server: {
+    open: "/index.html",
+    port: 3116,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                another: resolve(__dirname, 'another.html')
-            }
-        }
-    }
-}
+  },
+};
